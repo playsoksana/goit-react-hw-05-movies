3505127d1c4b54d64777076bbaaafa3f527f2d3d
playsoksana <playsoksana@gmail.com>
2021-08-03 14:38:08 +0300

    Updates

---
+++ static/js/Cards.840ec4b3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{59:function(t,i,e){"use strict";e.r(i),e.d(i,"default",(function(){return o}));e(0);var c=e(11),n=e(1),o=function(t){var i=t.title,e=t.originalName,o=t.id;return Object(n.jsx)("li",{children:Object(n.jsx)(c.b,{to:"/movies/".concat(o),children:i||e},o)})}}}]);
+//# sourceMappingURL=Cards.840ec4b3.chunk.js.map
